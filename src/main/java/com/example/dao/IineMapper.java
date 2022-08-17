8374fb0dd7c216cf2b0abb5803ecfddb39d8de04
nana-47 <nana.oikawa@rakus-partners.co.jp>
2022-08-17 17:53:26 +0900

    xmlの格納場所を修正

--- src/main/java/com/example/dao/IineMapper.java
+++ src/main/java/com/example/dao/IineMapper.java
@@ -1,4 +1,4 @@
-package com.example.mapper;
+package com.example.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
--- src/main/java/com/example/service/IineMapperService.java
+++ src/main/java/com/example/service/IineMapperService.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.dao.IineMapper;
 import com.example.domain.Iine;
-import com.example.mapper.IineMapper;
 
 @Transactional
 @Service
